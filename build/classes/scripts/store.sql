e1a6543a883ea02aeb759a9abc441bc5d9815a82
Sergio Garcia <1714110061@utec-tgo.edu.mx>
2016-10-31 15:23:08 -0600

    Agregada la tabla de usuarios a la base de datos.

--- build/classes/scripts/store.sql
+++ build/classes/scripts/store.sql
@@ -2,79 +2,94 @@ drop database if exists store;
 create database if not exists store;
 use store;
 
+create table if not exists usuarios (
+    nombre varchar(30) not null primary key,
+    usuario varchar(30) not null,
+    clave varchar(20) not null,
+    nivel varchar(20) not null,
+    estado varchar(10) not null
+);
+
 create table if not exists proveedores (
-	id_proveedor smallint(5) not null auto_increment primary key,
-	nombre varchar(50) not null,
-	rfc varchar(20) not null,
-	calle varchar(30) not null,
-	numero smallint(5) not null,
-	colonia varchar(30) not null,
-	ciudad varchar(30) not null, 
-	estado varchar(30) not null,
-	nombre_contacto varchar(50) not null,
-	telefono varchar(15) not null,
-	email varchar(30) not null 
+    id_proveedor smallint(5) not null auto_increment primary key,
+    nombre varchar(50) not null,
+    rfc varchar(20) not null,
+    calle varchar(30) not null,
+    numero smallint(5) not null,
+    colonia varchar(30) not null,
+    ciudad varchar(30) not null, 
+    estado varchar(30) not null,
+    nombre_contacto varchar(50) not null,
+    telefono varchar(15) not null,
+    email varchar(30) not null 
 );
 
 create table if not exists productos (
-	id_producto smallint(5) not null auto_increment primary key,
-	producto varchar(50) not null,
-	descripcion varchar(90) not null,
-	precio_compra float(6) not null,
-	precio_venta float(6) not null,
-	existencias smallint(5) not null 	
+    id_producto smallint(5) not null auto_increment primary key,
+    producto varchar(50) not null,
+    descripcion varchar(90) not null,
+    precio_compra float(6) not null,
+    precio_venta float(6) not null,
+    existencias smallint(5) not null 	
 );
 
 create table if not exists clientes (
-	id_cliente smallint(5) not null auto_increment primary key,
-	nombre varchar(50) not null,
-	ap_paterno varchar(30) not null,
-	ap_materno varchar(30) not null,
-	telefono varchar(15) not null,
-	email varchar(30) not null, 
-	rfc varchar(20) not null,
-	calle varchar(30) not null,
-	numero smallint(5) not null,
-	colonia varchar(30) not null,
-	ciudad varchar(30) not null,
-	estado varchar(30) not null		
+    id_cliente smallint(5) not null auto_increment primary key,
+    nombre varchar(50) not null,
+    ap_paterno varchar(30) not null,
+    ap_materno varchar(30) not null,
+    telefono varchar(15) not null,
+    email varchar(30) not null, 
+    rfc varchar(20) not null,
+    calle varchar(30) not null,
+    numero smallint(5) not null,
+    colonia varchar(30) not null,
+    ciudad varchar(30) not null,
+    estado varchar(30) not null		
 );
 
 create table if not exists ventas (
-	id_venta smallint(5) not null auto_increment primary key,
-	fecha date not null,
-	id_cliente smallint(5) not null references clientes(id_cliente),
-	subtotal float(6) not null,
-	iva tinyint(4) not null,
-	total float(6) not null 	
+    id_venta smallint(5) not null auto_increment primary key,
+    fecha date not null,
+    id_cliente smallint(5) not null references clientes(id_cliente),
+    subtotal float(6) not null,
+    iva tinyint(4) not null,
+    total float(6) not null 	
 );
 
 create table if not exists compras (
-	id_compra smallint(5) not null auto_increment primary key,
-	fecha date not null,
-	id_proveedor smallint(5) not null references proveedores(id_proveedor),
-	subtotal float(6) not null,
-	iva tinyint(4) not null,
-	total float(6) not null 	
+    id_compra smallint(5) not null auto_increment primary key,
+    fecha date not null,
+    id_proveedor smallint(5) not null references proveedores(id_proveedor),
+    subtotal float(6) not null,
+    iva tinyint(4) not null,
+    total float(6) not null 	
 );
 
 create table if not exists detalleVentas (
-	id_det_venta smallint(5) not null auto_increment primary key,
-	id_venta smallint(5) not null references ventas(id_venta),
-	id_producto smallint(5) not null references productos(id_producto),
-	cantidad int(10) not null,
-	total_prodcutos float(6) not null,
-	precio float(6) not null 	
+    id_det_venta smallint(5) not null auto_increment primary key,
+    id_venta smallint(5) not null references ventas(id_venta),
+    id_producto smallint(5) not null references productos(id_producto),
+    cantidad int(10) not null,
+    total_productos float(6) not null,
+    precio float(6) not null 	
 );
 
 create table if not exists detalleCompras (
-	id_det_compra smallint(5) not null auto_increment primary key,
-	id_compra smallint(5) not null references compras(id_compra),
-	id_producto smallint(5) not null references productos(id_producto),
-	cantidad int(10) not null,
-	total_prodcutos float(6) not null,
-	precio float(6) not null 	
+    id_det_compra smallint(5) not null auto_increment primary key,
+    id_compra smallint(5) not null references compras(id_compra),
+    id_producto smallint(5) not null references productos(id_producto),
+    cantidad int(10) not null,
+    total_productos float(6) not null,
+    precio float(6) not null 	
 );
+
+insert into usuarios(nombre, usuario, clave, nivel, estado)
+values('Sergio', 'MrButter21', 'Alessandra2000', 'Administrador', 'Activo');
+insert into usuarios(nombre, usuario, clave, nivel, estado)
+values('Alessandra', 'dead_alex12', 'EssO&R456', 'Cliente', 'Activo');
+insert into usuarios(nombre, usuario, clave, nivel, estado)
+values('Cornelio', 'CornieES', 'CornPops', 'Provvedor', 'Activo');
 
 insert into clientes(nombre, ap_paterno, ap_materno, telefono, email, rfc, calle, numero, colonia, ciudad, estado)
 values('Rubi', 'Vargas', 'Cruz', '7751171638', 'alex15@hotmail.com', '2394574103', 'Nogales', '210', 'Estrella', 'Tulancingo', 'Hidalgo');
